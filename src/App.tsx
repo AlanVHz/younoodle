5a6e9587460416646779313495873d23f85b4e69
Alan VH <alanvhz.09@gmail.com>
2024-02-15 16:03:55 -0600

    Dummy algorithm to have data and continue with extra goals

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,27 @@ function App() {
   const [investorsData, setInvestorsData] = useState<string[][]>([]);
   const [startupData, setStartupData] = useState<string[][]>([]);
 
+  const makeMatches = (investorsCopy: string[][], startupDataCopy: string[][]) => {
+    
+    const matches = [];
+
+    for (const investor of investorsCopy) {
+      let matchesCount = 0;
+      for (const startup of startupDataCopy) {
+        if (investor[1] === "any" || investor[1] === startup[1]) {
+          console.log(`investor: ${investor[0]} with industry: ${investor[1]} |  Startup: ${startup[0]} with industry: ${startup[1]}`)
+          matches.push({ investor: investor[0], startup: startup[0] });
+          matchesCount++;
+          if (matchesCount >= 10) {
+            break;
+          }
+        }
+      }
+    }
+
+    console.log(matches);
+  };
+
   useEffect(() => {
     const fetchCsvData = async () => {
       const investorsData = await fetchAndParseCSV("assets/csv/investors.csv");
@@ -13,6 +34,8 @@ function App() {
 
       const startupData = await fetchAndParseCSV("assets/csv/startups.csv");
       setStartupData(startupData);
+
+      makeMatches(investorsData, startupData);
     };
 
     fetchCsvData();
@@ -21,13 +44,21 @@ function App() {
   return (
     <>
       <ul>
-        { investorsData.map((item) => {
-          return <li key={item[0]}>Investor: {item[0]}, Industry: {item[1]} </li>
-        }) }
+        {investorsData.map((item) => {
+          return (
+            <li key={item[0]}>
+              Investor: {item[0]}, Industry: {item[1]}{" "}
+            </li>
+          );
+        })}
 
-        { startupData.map((item) => {
-          return <li key={item[0]}>Startup: {item[0]}, Industry: {item[1]} </li>
-        }) }
+        {startupData.map((item) => {
+          return (
+            <li key={item[0]}>
+              Startup: {item[0]}, Industry: {item[1]}{" "}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
