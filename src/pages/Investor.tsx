e26e5519985cef44d0758893d27a7e976d729003
Alan VH <alanvhz.09@gmail.com>
2024-02-19 23:50:33 -0600

    Input UI improvements

--- src/pages/Investor.tsx
+++ src/pages/Investor.tsx
@@ -141,7 +141,7 @@ function Investor() {
             </div>
             <div className='md:w-2/3'>
               <input
-                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:teal-500'
+                className='appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:teal-500'
                 id='inline-full-name'
                 type='text'
                 value={investorsName}
@@ -171,7 +171,7 @@ function Investor() {
                     name='industry'
                     value={investorsIndustry}
                     onChange={handleIndustryChange}
-                    className='py-2 px-4 bg-gray-200 border-2 border-gray-200 rounded w-full text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500'
+                    className='py-2 px-4 border-2 border-gray-200 rounded w-full text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500'
                   >
                     {industryValues.map((industry) => {
                       return <option key={industry}>{industry}</option>;
